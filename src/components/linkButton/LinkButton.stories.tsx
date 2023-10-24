c54e8ff377553bc1afda1f52eedc639e3225aa9e
William Salguero (Wesc~) <142819165+WescSalguero@users.noreply.github.com>
2023-10-24 11:57:18 -0600

    Added Label with icon with Environment Variables

--- src/components/linkButton/LinkButton.stories.tsx
+++ src/components/linkButton/LinkButton.stories.tsx
@@ -2,11 +2,17 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { LinkButton } from 'components-bullseye';
 
+const icons = { icon: "https://cdn-icons-png.flaticon.com/128/949/949647.png", size: 20 }
+
 const meta: Meta<typeof LinkButton> = {
     component: LinkButton,
     tags: ['autodocs'],
     title: 'Link Button',
-    argTypes: {
+    args: {
+        icon: icons,
+        title: "title",
+        url: "https://example.com"
+
     }
 }
 
@@ -20,8 +26,8 @@ const LinkButtonSampleArgs = {
 };
 
 export const CardWithImageStorybook: Story = {
-    render: (args, { globals: { showInCard } }) => 
-    <div className={showInCard === "Yes" ? "card shadow-sm p-3 mt-3" : ""}>
-        <LinkButton {...LinkButtonSampleArgs} />
-    </div>
+    render: (args, { globals: { showInCard } }) =>
+        <div className={showInCard === "Yes" ? "card shadow-sm p-3 mt-3" : ""}>
+            <LinkButton {...LinkButtonSampleArgs} />
+        </div>
 }
